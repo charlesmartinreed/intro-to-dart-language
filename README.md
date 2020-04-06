34a17252f44bfe00fb99b5217760a87c17c6ea8c
charlesmartinreed <charlesmartinreed@icloud.com>
2020-04-06 18:47:31 -0500

    Hello world main() example.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dart is...
+
+- **Statically** typed (except for the Dart-specific 'dynamic' type)
+- **Compiled**, either _ahead of time_ or _just in time_
+
+# Dart packages are...
+
+- Imported from `dart:core` package, automatically
---
+++ playground.dart
@@ -0,0 +1,10 @@
+// Every DART program begins with a main function
+
+// if the function doesn't return anything, we can just main(). But void is also used in this case as well.
+
+void main() {
+  var firstName = "Charles"; // inferred as String
+  String lastName = "Reed"; // explicitly defined as String
+
+  print('Hello, my name is ' + firstName + ' ' + lastName + '.');
+}
